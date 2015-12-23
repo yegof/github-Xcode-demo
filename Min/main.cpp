6c72ac0371316a0908e1102bcb2b61e85bb9c69b
yegof <yegofm@yahoo.com>
2015-12-22 20:50:09 -0800

    Initial Commit

---
+++ Min/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Min
+//
+//  Created by minyahil kebebegn on 12/22/15.
+//  Copyright © 2015 minyahil kebebegn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
